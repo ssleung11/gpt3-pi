317e734f5b62f4d7fd8e99ff3753d934d744232f
Shirley Leung <shirley@Shirleys-MacBook-Pro.local>
2022-09-27 20:35:11 -0400

    added back package accidentally deleted

--- example/setup.sh
+++ example/setup.sh
@@ -2,6 +2,7 @@
 pip3 install -r requirements.txt 
 sudo apt-get -y install libportaudio0 
 sudo apt-get -y install libportaudio2 
+sudo apt-get -y install mpg123
 sudo apt-get -y install libportaudiocpp0 
 sudo apt-get -y install portaudio19-dev
 sudo apt-get -y install python-pyaudio
