d1cef9bbe0edc54517aac9e5c39757fc3e60e4f3
Shirley Leung <shirley@Shirleys-MacBook-Pro.local>
2022-09-27 20:27:17 -0400

    updated package and changed chunk rate

--- example/setup.sh
+++ example/setup.sh
@@ -4,4 +4,7 @@ sudo apt-get -y install libportaudio0
 sudo apt-get -y install libportaudio2 
 sudo apt-get -y install libportaudiocpp0 
 sudo apt-get -y install portaudio19-dev
+sudo apt-get -y install python-pyaudio
+sudo apt-get -y install python3-pyaudio
 sudo apt-get -y install mpg123
+sudo apt-get -y install flac
